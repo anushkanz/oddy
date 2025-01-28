32c09c092866c42ca346b4dec283bed9efc5975d
anushkanz <vishal.dj.fa9@gmail.com>
2025-01-29 00:50:43 +1300

    create user collection migration

---
+++ database/migrations/2025_01_28_114938_create_users_collection.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersCollection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create the 'users' collection
+        Schema::create('users', function (Blueprint $collection) {
+            // Unique ID (automatically created by MongoDB)
+            $collection->index('_id');
+
+            // User details
+            $collection->string('name');
+            $collection->string('email')->unique();
+            $collection->string('password');
+            $collection->string('phone')->nullable();
+            $collection->string('role')->default('customer'); // 'customer' or 'admin'
+
+            // Timestamps
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop the 'users' collection
+        Schema::drop('users');
+    }
+}
