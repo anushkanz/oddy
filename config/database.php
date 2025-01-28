87c55f38c9971a00efc527b91370d256d2007881
anushkanz <vishal.dj.fa9@gmail.com>
2025-01-29 01:20:09 +1300

    chanage db config

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,7 +30,11 @@ return [
     */
 
     'connections' => [
-
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI'),
+            'database' => 'Oddydb',
+        ],
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
