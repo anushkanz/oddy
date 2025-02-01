9cdac240c0c4614e7b8e74c861f3d4a4875c3e71
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-01 17:30:16 +1300

    Student Section

--- resources/views/layouts/master_student.blade.php
+++ resources/views/layouts/master_student.blade.php
@@ -15,34 +15,7 @@
   </div>
   <div class="navbar-menu" id="navbar-menu">
     <div class="navbar-end">
-      <div class="navbar-item dropdown has-divider">
-        <a class="navbar-link">
-          <span class="icon"><i class="mdi mdi-menu"></i></span>
-          <span>Sample Menu</span>
-          <span class="icon">
-            <i class="mdi mdi-chevron-down"></i>
-          </span>
-        </a>
-        <div class="navbar-dropdown">
-          <a href="profile.html" class="navbar-item">
-            <span class="icon"><i class="mdi mdi-account"></i></span>
-            <span>My Profile</span>
-          </a>
-          <a class="navbar-item">
-            <span class="icon"><i class="mdi mdi-settings"></i></span>
-            <span>Settings</span>
-          </a>
-          <a class="navbar-item">
-            <span class="icon"><i class="mdi mdi-email"></i></span>
-            <span>Messages</span>
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            <span class="icon"><i class="mdi mdi-logout"></i></span>
-            <span>Log Out</span>
-          </a>
-        </div>
-      </div>
+      
       <div class="navbar-item dropdown has-divider has-user-avatar">
         <a class="navbar-link">
           <div class="user-avatar">
@@ -66,7 +39,7 @@
           </a>
           <hr class="navbar-divider">
           <a class="navbar-item">
-            <span class="icon"><i class="mdi mdi-logout"></i></span>
+            <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
             <span>Log Out</span>
           </a>
         </div>
@@ -80,7 +53,7 @@
         <span>GitHub</span>
       </a>
       <a title="Log out" class="navbar-item desktop-icon-only">
-        <span class="icon"><i class="mdi mdi-logout"></i></span>
+        <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
         <span>Log out</span>
       </a>
     </div>
