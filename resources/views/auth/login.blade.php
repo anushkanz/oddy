9462b8ce16a8e69dfeff13058f3b092cf59dbaba
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-01 16:47:13 +1300

    Force to use https

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,70 @@
 @extends('layouts.master_open_header')
 @section('content')
-<div class="container mx-auto auth-pages">
-    <div class="row justify-content-center">
-        <div class="width-60z">
-            <div class="card">
-                <h1 class="mb-[40px]">Login.</h1>
-                <p class="login-text-z">Login to your customer or transporter account to gain access to your dashboard where you can find all the relevant information needed to ship or drive. </p>
-                <div class="card-body">
-                    <p class="pre-form-text">Your details</p>
-                    <form class="common-form-z login-form-z" method="POST" action="{{ route('login.custom') }}">
-                @csrf <!-- {{ csrf_field() }} -->
-                        <div class="form-group mb-3">
-                            <input type="text" placeholder="Email" id="email" class="form-control" name="email" required autofocus>
-                        </div>
+<div id="app">
 
-                        <div class="form-group mb-3">
-                            <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
-                            @if ($errors->has('emailPassword'))
-                            <span class="text-danger">{{ $errors->first('emailPassword') }}</span>
-                            @endif
-                        </div>
-
-                        <div class="form-group mb-3 text-black mt-4">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember"> Remember Me
-                                </label>
-                            </div>
-                        </div>
-
-                        <div class="login-btns">
-                            <div class="input-btn-a"><a href="/forget_password" type="submit">Password reset</a></div>
-                            <button type="submit" class="input-btn-red">Signin</button>
-                        </div>
-                    </form>
-
-                </div>
+  <section class="section main-section">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          <span class="icon"><i class="mdi mdi-lock"></i></span>
+          Login
+        </p>
+      </header>
+      <div class="card-content">
+      <form method="POST" action="{{ route('login.custom') }}">
+        @csrf <!-- {{ csrf_field() }} -->
+            <div class="field spaced">
+            <label class="label">Login</label>
+            <div class="control icons-left">
+              <input class="input" type="text"  id="email" placeholder="user@example.com" autocomplete="username"  required autofocus>
+              <span class="icon is-small left"><i class="mdi mdi-account"></i></span>
             </div>
-        </div>
+            <p class="help">
+              Please enter your login
+            </p>
+            </div>
+
+            <div class="field spaced">
+            <label class="label">Password</label>
+            <p class="control icons-left">
+                <input type="password" placeholder="Password" id="password" class="input" name="password" required>
+                <span class="icon is-small left"><i class="mdi mdi-asterisk"></i></span>
+                @if ($errors->has('emailPassword'))
+                    <span class="text-danger">{{ $errors->first('emailPassword') }}</span>
+                @endif
+            </p>
+            <p class="help">
+              Please enter your password
+            </p>
+            </div>
+
+            <div class="field spaced">
+            <div class="control">
+              <label class="checkbox"><input type="checkbox" name="remember" value="1" checked>
+                <span class="check"></span>
+                <span class="control-label">Remember</span>
+              </label>
+            </div>
+            </div>
+
+          <hr>
+
+          <div class="field grouped">
+            <div class="control">
+              <button type="submit" class="button blue">
+                Login
+              </button>
+            </div>
+            
+          </div>
+
+        </form>
+      </div>
     </div>
+
+  </section>
+
+
 </div>
+
 @endsection
