7cfba3258aad38c0169bcbbb0e29e116d07e3f7d
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-01 17:43:22 +1300

    Student Section

--- resources/views/layouts/master_student.blade.php
+++ resources/views/layouts/master_student.blade.php
@@ -19,10 +19,10 @@
       <div class="navbar-item dropdown has-divider has-user-avatar">
         <a class="navbar-link">
           <div class="user-avatar">
-            <img src="https://avatars.dicebear.com/v2/initials/john-doe.svg" alt="John Doe" class="rounded-full">
+            <img src="" alt="John Doe" class="rounded-full">
           </div>
           <div class="is-user-name"><span>John Doe</span></div>
-          <span class="icon"><i class="mdi mdi-chevron-down"></i></span>
+          <span class="icon"><i class="fa-solid fa-chevron-down"></i></span>
         </a>
       </div>
       <a href="#" class="navbar-item has-divider desktop-icon-only">
@@ -33,7 +33,7 @@
         <span class="icon"><i class="fa-regular fa-circle-question"></i></span>
         <span>GitHub</span>
       </a>
-      <a title="Log out" class="navbar-item desktop-icon-only">
+      <a href="{{signout}}" title="Log out" class="navbar-item desktop-icon-only">
         <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
         <span>Log out</span>
       </a>
@@ -51,69 +51,41 @@
     <ul class="menu-list">
       <li class="--set-active-index-html">
         <a href="index.html">
-          <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
+          <span class="icon"><i class="fa-solid fa-display"></i></span>
           <span class="menu-item-label">Dashboard</span>
         </a>
       </li>
-    </ul>
-    <p class="menu-label">Examples</p>
-    <ul class="menu-list">
       <li class="--set-active-tables-html">
-        <a href="tables.html">
-          <span class="icon"><i class="mdi mdi-table"></i></span>
-          <span class="menu-item-label">Tables</span>
+        <a href="#">
+          <span class="icon"><i class="fa-solid fa-tower-observation"></i></span>
+          <span class="menu-item-label">Bookings</span>
         </a>
       </li>
-      <li class="--set-active-forms-html">
-        <a href="forms.html">
-          <span class="icon"><i class="mdi mdi-square-edit-outline"></i></span>
-          <span class="menu-item-label">Forms</span>
+      <li class="--set-active-tables-html">
+        <a href="#">
+          <span class="icon"><i class="fa-regular fa-comment"></i></span>
+          <span class="menu-item-label">Reviews</span>
         </a>
       </li>
-      <li class="active">
-        <a href="profile.html">
-          <span class="icon"><i class="mdi mdi-account-circle"></i></span>
-          <span class="menu-item-label">Profile</span>
+      <li class="--set-active-tables-html">
+        <a href="#">
+          <span class="icon"><i class="fa-regular fa-user"></i></span>
+          <span class="menu-item-label">Account</span>
         </a>
-      </li>
-      <li>
-        <a href="login.html">
-          <span class="icon"><i class="mdi mdi-lock"></i></span>
-          <span class="menu-item-label">Login</span>
-        </a>
-      </li>
-      <li>
-        <a class="dropdown">
-          <span class="icon"><i class="mdi mdi-view-list"></i></span>
-          <span class="menu-item-label">Submenus</span>
-          <span class="icon"><i class="mdi mdi-plus"></i></span>
-        </a>
-        <ul>
-          <li>
-            <a href="#void">
-              <span>Sub-item One</span>
-            </a>
-          </li>
-          <li>
-            <a href="#void">
-              <span>Sub-item Two</span>
-            </a>
-          </li>
-        </ul>
       </li>
     </ul>
     <p class="menu-label">About</p>
     <ul class="menu-list">
       <li>
-        <a href="https://justboil.me/tailwind-admin-templates/free-dashboard/" class="has-icon">
-          <span class="icon"><i class="mdi mdi-help-circle"></i></span>
+        <a href="#" class="has-icon">
+          <span class="icon"><i class="fa-regular fa-address-card"></i></span>
           <span class="menu-item-label">About</span>
         </a>
       </li>
       <li>
-        <a href="https://github.com/justboil/admin-one-tailwind" class="has-icon">
-          <span class="icon"><i class="mdi mdi-github-circle"></i></span>
-          <span class="menu-item-label">GitHub</span>
+        <a href="{{signout}}" class="has-icon">
+          <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
+          <span class="menu-item-label">Sign Out</span>
         </a>
       </li>
     </ul>
