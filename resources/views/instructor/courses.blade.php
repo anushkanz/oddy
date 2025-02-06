24e646b78c292e7d0e6a7b739bd5177b4048487d
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-07 12:02:08 +1300

    Course Form date and time

--- resources/views/instructor/courses.blade.php
+++ resources/views/instructor/courses.blade.php
@@ -75,6 +75,19 @@
                   </div>
                 </div>
                 <div class="field">
+                  <label class="label">Course Level</label>
+                  <div class="control">
+                    <div class="select">
+                      <select name="category">
+                          <option value="beginner">Beginner</option>
+                          <option value="intermediate">Intermediate</option>
+                          <option value="advance">Advance</option>
+                      </select>
+                      <p class="help">Required. Course category</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="field">
                   <label class="label">Course Images</label>
                   <div class="field-body">
                     <div class="field file">
@@ -141,81 +154,10 @@
             </div>
             <p class="help">Required. Course country</p>
           </div>
-          <hr>
-          <div class="field">
-            <div class="control">
-              <button type="submit" class="button green">
-                Submit
-              </button>
-            </div>
-          </div>
+
+          
         </div>
       </div>
-
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">
-            <span class="icon"><i class="mdi mdi-account-circle"></i></span>
-            Location Details
-          </p>
-        </header>
-        <div class="card-content">
-          <div class="field">
-            <label class="label">Location Selection</label>
-            <div class="control">
-              <div class="select">
-                <select name="location">
-                  <option value="">Select Location</option>
-                  @foreach($locations as $location)
-                    <option value="{{$location->_id}}">{{$location->name}}</option>
-                  @endforeach  
-                </select>
-                <p class="help">You can select previous location</p>
-              </div>
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Location Name</label>
-            <div class="control">
-              <input type="text" autocomplete="on" name="location_name" value="" class="input" required>
-            </div>
-            <p class="help">Required. Course Location name</p>
-          </div>
-   
-          <div class="field">
-            <label class="label">Address</label>
-            <div class="control">
-            <input type="text" autocomplete="on" name="location_address" value="" class="input" required>
-            </div>
-            <p class="help">Required. Course address</p>
-          </div>
-     
-          <div class="field">
-            <label class="label">City</label>
-            <div class="control">
-            <input type="text" autocomplete="on" name="location_city" value="" class="input" required>
-            </div>
-            <p class="help">Required. Course City</p>
-          </div>
-          <div class="field">
-            <label class="label">Country</label>
-            <div class="control">
-            <input type="text" autocomplete="on" name="location_country" readonly value="NZ" class="input" required>
-            </div>
-            <p class="help">Required. Course country</p>
-          </div>
-          <hr>
-          <div class="field">
-            <div class="control">
-              <button type="submit" class="button green">
-                Submit
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
 
 
       <div class="card">
@@ -256,21 +198,7 @@
             </div>
             <p class="help">Required. Course date and time</p>
           </div>
-     
-          <div class="field">
-            <label class="label">City</label>
-            <div class="control">
-            <input type="text" autocomplete="on" name="location_city" value="" class="input" required>
-            </div>
-            <p class="help">Required. Course City</p>
-          </div>
-          <div class="field">
-            <label class="label">Country</label>
-            <div class="control">
-            <input type="text" autocomplete="on" name="location_country" readonly value="NZ" class="input" required>
-            </div>
-            <p class="help">Required. Course country</p>
-          </div>
+
           <hr>
           <div class="field">
             <div class="control">
