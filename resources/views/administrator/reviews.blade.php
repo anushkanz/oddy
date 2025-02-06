157295f3860e4a19a1e96f1165897345a856e8b9
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-07 01:11:56 +1300

    Student Dashboard

--- resources/views/administrator/reviews.blade.php
+++ resources/views/administrator/reviews.blade.php
@@ -9,65 +9,18 @@
       <header class="card-header">
         <p class="card-header-title">
           <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-          Reviews Given
+          Reviews
         </p>
         <a href="#" class="card-header-icon">
           <span class="icon"><i class="mdi mdi-reload"></i></span>
         </a>
       </header>
       <div class="card-content">
-        <table id="reviewer">
+        <table id="courses">
           <thead>
           <tr>
             <th></th>
             <th>Receiver</th>
-            <th>Course</th>
-            <th>Ratings</th>
-            <th></th>
-          </tr>
-          </thead>
-          <tbody>
-          @php
-            foreach($reviewer as $review){
-          @endphp
-            <tr>
-              <td class="image-cell"></td>
-              <td data-label="Name" class="--name">{{$review->receiver()->name}}</td>
-              <td data-label="Title" class="--title">{{$review->reviewer()->title}}</td>
-              <td data-label="Category" class="--category">{{$review->classes()->title}}</td>
-              <td data-label="Status" class="--status">{{$review->rating}}</td>
-              <td class="actions-cell">
-                <div class="buttons right nowrap">
-                  <a href="{{ route('administrator.review',[$review->_id]) }}" class="button small blue --jb-modal"  data-target="sample-modal-2" type="button">
-                    <span class="icon"><i class="fa-regular fa-eye"></i></span>
-                  </a>
-                </div>
-              </td>
-            </tr>
-          @php
-            }
-          @endphp
-          </tbody>
-        </table>
-        
-      </div>
-    </div>
-
-    <div class="card has-table">
-      <header class="card-header">
-        <p class="card-header-title">
-          <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-          Reviews Got
-        </p>
-        <a href="#" class="card-header-icon">
-          <span class="icon"><i class="mdi mdi-reload"></i></span>
-        </a>
-      </header>
-      <div class="card-content">
-        <table id="reviewer">
-          <thead>
-          <tr>
-            <th></th>
             <th>Reviewer</th>
             <th>Course</th>
             <th>Ratings</th>
@@ -76,14 +29,13 @@
           </thead>
           <tbody>
           @php
-            foreach($receiver as $review){
+            foreach($reviews as $review){
           @endphp
             <tr>
               <td class="image-cell"></td>
-              <td data-label="Name" class="--name">{{$review->receiver()->name}}</td>
-              <td data-label="Title" class="--title">{{$review->reviewer()->title}}</td>
-              <td data-label="Category" class="--category">{{$review->classes()->title}}</td>
-              <td data-label="Status" class="--status">{{$review->rating}}</td>
+              <td data-label="Name" class="--name">{{$review->review()->name}}</td>
+              <td data-label="Title" class="--title">{{$review->classes()->title}}</td>
+              <td data-label="Category" class="--category">{{$review->rating}}</td>
               <td class="actions-cell">
                 <div class="buttons right nowrap">
                   <a href="{{ route('administrator.review',[$review->_id]) }}" class="button small blue --jb-modal"  data-target="sample-modal-2" type="button">
@@ -103,7 +55,7 @@
     <script type="text/javascript">
 
       $(function() {
-          $('#reviewer').DataTable( {
+          $('#courses').DataTable( {
               paging: true,
               responsive: true,
               pageLength: 10
--- resources/views/instructor/reviews.blade.php
+++ resources/views/instructor/reviews.blade.php
@@ -8,19 +8,18 @@
       <header class="card-header">
         <p class="card-header-title">
           <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-          Reviews
+          Reviews Given
         </p>
         <a href="#" class="card-header-icon">
           <span class="icon"><i class="mdi mdi-reload"></i></span>
         </a>
       </header>
       <div class="card-content">
-        <table id="reviews">
+        <table id="reviewer">
           <thead>
           <tr>
             <th></th>
             <th>Receiver</th>
-            <th>Reviewer</th>
             <th>Course</th>
             <th>Ratings</th>
             <th></th>
@@ -28,7 +27,7 @@
           </thead>
           <tbody>
           @php
-            foreach($reviews as $review){
+            foreach($reviewer as $review){
           @endphp
             <tr>
               <td class="image-cell"></td>
@@ -38,7 +37,55 @@
               <td data-label="Status" class="--status">{{$review->rating}}</td>
               <td class="actions-cell">
                 <div class="buttons right nowrap">
-                  <a href="{{ route('instructor.review',[$review->_id]) }}" class="button small blue --jb-modal"  data-target="sample-modal-2" type="button">
+                  <a href="{{ route('administrator.review',[$review->_id]) }}" class="button small blue --jb-modal"  data-target="sample-modal-2" type="button">
+                    <span class="icon"><i class="fa-regular fa-eye"></i></span>
+                  </a>
+                </div>
+              </td>
+            </tr>
+          @php
+            }
+          @endphp
+          </tbody>
+        </table>
+        
+      </div>
+    </div>
+
+    <div class="card has-table">
+      <header class="card-header">
+        <p class="card-header-title">
+          <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
+          Reviews Got
+        </p>
+        <a href="#" class="card-header-icon">
+          <span class="icon"><i class="mdi mdi-reload"></i></span>
+        </a>
+      </header>
+      <div class="card-content">
+        <table id="reviewer">
+          <thead>
+          <tr>
+            <th></th>
+            <th>Reviewer</th>
+            <th>Course</th>
+            <th>Ratings</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          @php
+            foreach($receiver as $review){
+          @endphp
+            <tr>
+              <td class="image-cell"></td>
+              <td data-label="Name" class="--name">{{$review->receiver()->name}}</td>
+              <td data-label="Title" class="--title">{{$review->reviewer()->title}}</td>
+              <td data-label="Category" class="--category">{{$review->classes()->title}}</td>
+              <td data-label="Status" class="--status">{{$review->rating}}</td>
+              <td class="actions-cell">
+                <div class="buttons right nowrap">
+                  <a href="{{ route('administrator.review',[$review->_id]) }}" class="button small blue --jb-modal"  data-target="sample-modal-2" type="button">
                     <span class="icon"><i class="fa-regular fa-eye"></i></span>
                   </a>
                 </div>
@@ -55,7 +102,7 @@
     <script type="text/javascript">
 
       $(function() {
-          $('#reviews').DataTable( {
+          $('#reviewer').DataTable( {
               paging: true,
               responsive: true,
               pageLength: 10
