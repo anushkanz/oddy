03e4572e397c894320240d87cab5ec5140553949
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-08 22:32:06 +1300

    Update course

--- resources/views/instructor/course.blade.php
+++ resources/views/instructor/course.blade.php
@@ -327,8 +327,8 @@
                                     "_token": "{{ csrf_token() }}",
                                 },
                                 success: function (response) {
-                                    console.log(response);
-                                    if(response.data.message == 'deleted'){
+                                    console.log(response.status);
+                                    if(response.data.status == 'true'){
                                        
                                         Swal.fire("Deleted!", "", "success");
                                        
