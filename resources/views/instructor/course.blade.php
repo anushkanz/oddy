dda3e0ff8ecb3220eda8f921e971bf8703e45a32
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-08 14:00:26 +1300

    Update course

--- resources/views/instructor/course.blade.php
+++ resources/views/instructor/course.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.master_instructor')
 
 @section('title', 'Home Page')
-@php
-dd($course->location)
-@endphp
+
 @section('content')
 
 <form method='post'  enctype='multipart/form-data'  action="{{ route('instructor.course.update') }}">
