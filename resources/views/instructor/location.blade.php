434e34354a4a7233f938105fed1e94e1e3d4e922
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-13 22:09:55 +1300

    Update course

--- resources/views/instructor/location.blade.php
+++ resources/views/instructor/location.blade.php
@@ -8,6 +8,7 @@
 @csrf
 <input type='hidden' name='task' value="update">
 <input type='hidden' name='user' value="{{$user->_id}}">
+<input type='hidden' name='id' value="{{ isset($location->_id) ? $location->_id : '' }}">
 <section class="section main-section">
   <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
       <div class="card">
