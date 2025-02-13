eb2bf48e68fe954c58c3b2c566e65b65dce8609a
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-13 22:04:09 +1300

    Update course

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -519,7 +519,7 @@ class InstructorController extends Controller
                     return redirect()->route('instructor.locations')->with('error','Unable to validate your data');
                 }
                 $location =  Location::where('user_id', $user->_id)->where('_id', $request->id)->get();
-                $location->name = $user->location_name;
+                $location->name = $request->location_name;
                 $location->address = $request->location_address;
                 $location->city = $request->location_city;
                 $location->update();
