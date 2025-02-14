253eae08306f88f221a656dd33c410ba24515100
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-14 14:12:22 +1300

    Update course

--- resources/views/instructor/image.blade.php
+++ resources/views/instructor/image.blade.php
@@ -23,15 +23,81 @@
               </div>
             </div>
             <div class="grid grid-cols-2 md:grid-cols-4 gap-4 listing-images-z">
+            <input type='hidden' name='user' value="{{$course_images->_id}}">
+            <input type='hidden' name='id' value="{{$course->id}}">
+            <input type="hidden" id="token" name="token" value="{{csrf_token()}}"/>
                 @foreach(json_decode($course_images->photo_gallery,true) as $imges)
-                    <div class="image"><img src="{{$imges['path']}}"></div>
+                    <div class="image">
+                    <input type="checkbox" id="removelist" name="removelist" value="{{$imges['name']}}" class="remove_list" />
+                        <img src="{{$imges['path']}}">
+                    </div>
                 @endforeach    
             </div>
  
           </div>
         </div>
       </div>
+      <hr>
+        <div class="field">
+        <div class="control">
+            <a id="deleteButton" type="submit" class="button red">
+                Delete Images
+            </a>
+        </div>
+        </div>
     </div>
 </div>
+<script>
+        $(document).ready(function(){
 
+            $('#deleteButton').click(function(e) {
+                e.preventDefault();
+                let array = []; 
+                $("input:checkbox[name=removelist]:checked").each(function() { 
+                    array.push($(this).val()); 
+                });
+            
+                var token =  $("input[name=token]").val();
+                var id =  $("input[name=id]").val();
+            
+                console.log(array);
+                
+                if(array.length > 0){
+                    $.ajax({
+                        type:'POST',
+                        url:"{{ route('instructor.image.ajax') }}",
+                        data:{
+                            _token:token, 
+                            listingId:id,
+                            images:array
+                        },
+                        success:function(response) {
+                            if (response.status === 'success') {
+                                Swal.fire({
+                                    icon: "success",
+                                    title: "Update message",
+                                    text: "You have successfully deleted images.",
+                                    showCancelButton: false,
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        location.reload();
+                                    }
+                                });    
+                            }
+                        }
+                    });
+                }else{
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error message",
+                        text: "You have not select any image.",
+                        showCancelButton: false,
+                    }).then((result) => {
+                        
+                    });   
+                }
+                 
+            });
+        });
+    </script>
 @endsection
