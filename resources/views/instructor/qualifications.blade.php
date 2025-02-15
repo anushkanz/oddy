f6e095c17df6a9f7a07990e2747bd307e274625f
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-16 00:01:01 +1300

    Update course

--- resources/views/instructor/qualifications.blade.php
+++ resources/views/instructor/qualifications.blade.php
@@ -14,7 +14,6 @@
 
 <section class="section main-section">
     <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
-
     <div class="card">
       <header class="card-header">
         <p class="card-header-title">
@@ -26,17 +25,49 @@
       <form method='post' enctype="multipart/form-data" action="{{ route('instructor.qualification.update') }}">
       <input type='hidden' name='task' value="create">
       <input type='hidden' name='user' value="{{$user->_id}}">
-          @csrf
-          <div id="fieldsContainer"> <!-- Dynamic fields will be added here --></div>
-          <button type="button" class="button blue" id="addFieldBtn">Add Qulification</button>
-          <hr>
-          <div class="field">
-            <div class="control">
-              <button type="submit" class="button green">
-                Submit
-              </button>
+            @csrf
+
+            <div class="field">
+                <label class="label">Name</label>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                    <input type="text" name="title" class="input" style="width: 100%;" required>
+                    </div>
+                    <p class="help">Required. Your name</p>
+                  </div>
+                </div>
+            </div> 
+            <div class="field">
+                <label class="label">Description</label>
+                <div class="field-body">
+                  <div class="field">
+                    <div class="control">
+                    <textarea type="text" rows="4" cols="50" autocomplete="on" name="description" value="" class="input" required></textarea>
+                    </div>
+                    <p class="help">Required. Your phone</p>
+                  </div>
+                </div>
             </div>
-          </div>
+            
+            <div class="field-body">
+                <div class="field file">
+                    <label class="upload control">
+                        <a class="button blue">
+                            Upload
+                        </a>
+                        <input type="file" name="file_upload[]">
+                    </label>
+                </div>
+            </div>
+            <hr>
+            <div class="field">
+                <div class="control">
+                <button type="submit" class="button green">
+                    Submit
+                </button>
+                </div>
+            </div>
         </form>
       </div>
     </div>
@@ -96,49 +127,7 @@
                 pageLength: 10
             });
 
-            $("#addFieldBtn").click(function () {
-                let fieldHtml = `
-                    <div class="field-container">
-                        <div class="field">
-                            <label class="label">Title</label>
-                            <div class="control">
-                            <input type="text" name="title[]" class="input" style="width: 100%;" required>
-                            </div>  
-                        </div>  
-                        <div class="field">  
-                            <label class="label">Description</label>
-                            <div class="control">
-                            <textarea type="text" rows="4" cols="50" autocomplete="on" name="description[]" value="" class="input" required></textarea>
-                            </div>
-                        </div>  
-                        <div class="field">
-                            <label class="label">Profile Images</label>
-                            <div class="field-body">
-                                <div class="field file">
-                                    <label class="upload control">
-                                        <a class="button blue">
-                                            Upload
-                                        </a>
-                                        <input type="file" name="file_upload[]">
-                                    </label>
-                                </div>
-                            </div>
-                        </div> 
-                        <div class="field">  
-                            <div class="control">
-                                <button type="button" class="remove-btn button red">Remove</button>
-                            </div>
-                        </div>
-                        <hr>  
-                    </div>
-                `;
-                $("#fieldsContainer").append(fieldHtml);
-            });
-
-            //Remove class dates
-            $(document).on("click", ".remove-btn", function () {
-                $(this).closest(".field-container").remove();
-            });
+            
 
 
             $(".class_qulification_edit").click(function (e) {
