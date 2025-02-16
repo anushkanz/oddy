d48cdcd84195273edb250c29912114d04d061d0e
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-16 22:33:58 +1300

    Booking Process

--- resources/views/booking/cart.blade.php
+++ resources/views/booking/cart.blade.php
@@ -79,7 +79,7 @@
                         <p>Class fee : $ {{$booking->classes->price}}</p>
                             @php 
                                 $fee_percentage = 0.963; // Change this value to set the desired fee percentage
-                                $payment_processing_fee =  (($amount + 0.3)/0.963) - $booking->classes->price;
+                                $payment_processing_fee =  (($booking->classes->price + 0.3)/0.963) - $booking->classes->price;
                                 $charge = round($booking->classes->price + $payment_processing_fee, 2);         
                             @endphp
                         <p>Booking fee : $ {{$payment_processing_fee}}</p>
