59be94044203f680d53bf5d51ef85d2c08110ca0
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-16 23:18:05 +1300

    Booking Process

--- resources/views/booking/checkout.blade.php
+++ resources/views/booking/checkout.blade.php
@@ -58,7 +58,7 @@
                                         $payment_processing_fee =  (($booking->classes->price + 0.3)/0.963) - $booking->classes->price;
                                         $charge = round($booking->classes->price + $payment_processing_fee, 2);         
                                     @endphp
-                                <input type="text" autocomplete="on" name="amount" value="{{ $charge }}" class="input" required>
+                                <input type="text" autocomplete="on" name="amount" value="{{ $charge }}" class="input" required disabled>
                             </div>
                         </div>
                     </div>
