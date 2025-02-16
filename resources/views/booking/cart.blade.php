bf5196b57094d93a53dd1b7015aba3dabe76d1a3
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-16 21:46:42 +1300

    Booking Process

--- resources/views/booking/cart.blade.php
+++ resources/views/booking/cart.blade.php
@@ -1,11 +1,42 @@
 @extends('layouts.master_open_header')
 @section('content')
 <section class="is-hero-bar">
-  <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
-    <h1 class="title">
-      Cart
-    </h1>
-    <button class="button light">Button</button>
-  </div>
+    <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
+        <h1 class="title">
+            Cart
+        </h1>
+    </div>
 </section>
+<div class="grid gap-6 grid-cols-1 md:grid-cols-3 mb-6">
+    <div class="card">
+        <div class="card-content">
+            <div class="flex items-center justify-between">
+                <div class="widget-label">
+                    <h3>
+                        Clients
+                    </h3>
+                    <h1>
+                        512
+                    </h1>
+                </div>
+                <span class="icon widget-icon text-green-500"><i class="mdi mdi-account-multiple mdi-48px"></i></span>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-content">
+            <div class="flex items-center justify-between">
+                <div class="widget-label">
+                    <h3>
+                        Sales
+                    </h3>
+                    <h1>
+                        $7,770
+                    </h1>
+                </div>
+                <span class="icon widget-icon text-blue-500"><i class="mdi mdi-cart-outline mdi-48px"></i></span>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
