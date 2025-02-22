ea210293d03bbc03a3b4251ca2e297b7369d1a01
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-22 19:40:35 +1300

    Booking Process

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -93,15 +93,9 @@ class StudentController extends Controller
             try {
                 $user = Auth::user();
                 $booking = Booking::where('user_id', $user->_id)->where('_id', $booking_id)->firstOrFail();
-                // dd($booking);
-                // if (!$booking->isEmpty()) {
-                //     return view('student.review', compact('review','user'));
-                // }
+                return view('student.review', compact('review','user'));
             } catch(\Exception $exception) {
-                dd($exception->getMessage());
-                // else{
-                //     return redirect()->route('error.error')->with('error-page','Unable to find your request');
-                // }
+                return redirect()->route('error.error')->with('error-page','Unable to find your request');
             }    
         }
     }
