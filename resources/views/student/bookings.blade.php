58f04f08652aa5962a138160e9e7818e68b82d59
anushkanz <vishal.dj.fa9@gmail.com>
2025-02-22 15:00:21 +1300

    Booking Process

--- resources/views/student/bookings.blade.php
+++ resources/views/student/bookings.blade.php
@@ -37,9 +37,17 @@
               <td class="image-cell"></td>
               <td data-label="Name" class="--name">{{$booking->user->name}}</td>
               <td data-label="Title" class="--title">{{$booking->classes->title}}</td>
-              <td data-label="Category" class="--category">{{$booking->payment->_id}}</td>
-              <td data-label="Status" class="--status">{{$booking->status}}</td>
-              <td data-label="date" class="--date">{{$booking->booking_date}}</td>
+              <td data-label="Category" class="--Seats">{{$booking->seat_count}}</td>
+              <td data-label="Status" class="--status">
+                @php 
+                  if($booking->status == 1){
+                      echo 'Successfull booked';
+                  }else{
+                      echo 'Unsuccessfull';
+                  }
+                @endphp
+              </td>
+              <td data-label="date" class="--date">{{$booking->created_at}}</td>
               <td class="actions-cell">
                 <div class="buttons right nowrap">
                   <a href="{{ route('student.booking',[$booking->_id]) }}" class="button small blue --jb-modal"  data-target="sample-modal-2" type="button">
